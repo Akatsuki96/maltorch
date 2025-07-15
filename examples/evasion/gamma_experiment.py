97b0cf66abd04370a8c7f6e48987f41d8a907a3f
Akatsuki96 <marco.rando0396@gmail.com>
2025-07-15 13:29:07 +0200

    Bug fix gamma experiment

--- examples/evasion/gamma_experiment.py
+++ examples/evasion/gamma_experiment.py
@@ -37,35 +37,37 @@ num_processes = int(sys.argv[7])
 os.makedirs(out_dir, exist_ok=True)
 
 budget = 500
-file_paths = [path for path in malware_folder.glob("*.dat")]
+file_paths = [path for path in malware_folder.glob("*.dat")]#[:10]
+
+
+def create_model(name, threshold):
+    if name == "malconv":
+        return MalConv.create_model(threshold=threshold)
+    elif name == "avast_style_conv":
+        return AvastStyleConv.create_model(threshold=threshold)
+    elif name == "ember_gbdt":
+        return EmberGBDT.create_model(threshold=threshold)
+    elif name == "bbdnn":
+        return BBDnn.create_model(threshold=threshold)
+    raise ValueError(f"Unknown model name: {name}")
 
 
 
-
-
-
-networks = {
-   'malconv': MalConv.create_model(threshold=threshold),
-   'avast_style_conv': AvastStyleConv.create_model(threshold=threshold),
-   'ember_gbdt' : EmberGBDT.create_model(threshold=threshold),
-   'bbdnn': BBDnn.create_model(threshold=threshold),
-}
-model = networks[model_name]
 reps = 5
+attack = GAMMASectionInjection(
+    query_budget=budget,
+    benignware_folder= Path(goodware_dir),#exe_folder / ".." / "benignware",
+    which_sections=[".text"],
+    how_many_sections=num_sections
+)
+model = create_model(model_name, threshold) 
+
+
 
 def run_experiment(file_path):
     for r in range(reps):
         print(f"[{r}/{reps}] Attacking {file_path}...")
-
-        attack = GAMMASectionInjection(
-            query_budget=budget,
-            benignware_folder= Path(goodware_dir),#exe_folder / ".." / "benignware",
-            which_sections=[".text"],
-            how_many_sections=num_sections
-        )
-
-
-        with open(f"{out_dir}/gamma_{str(file_path).split('/')[-1].split('.')[0]}_{model_name}_{num_sections}.txt", "w") as f:
+        with open(f"{out_dir}/gamma_{str(file_path).split('/')[-1].split('.')[0]}_{model_name}_{num_sections}.txt", "a") as f:
             x = load_single_exe(file_path).reshape(1, -1).long()
             y = create_labels(x, 1, device=device)
             dl = DataLoader(TensorDataset(x, y), batch_size=1)
@@ -73,20 +75,21 @@ def run_experiment(file_path):
             print("- - - Pre-attack accuracy: ", Accuracy()(model, dl).item(), pre_score, model.threshold)
             adv_dl = attack(model, dl)
             accuracy = Accuracy()(model, adv_dl)
-            print("Accuracy: ", accuracy)
             plain_length = x.flatten().shape[0]
             for (x_adv, y_adv) in adv_dl:
                 score =  model(x_adv).item()
                 adv_length = x_adv.flatten().shape[0]
                 print("Score: ", score, y_adv)
-                print("ADDED BYTES", x.flatten().shape[0], x_adv.flatten().shape[0])
+                print("ADDED BYTES", x_adv.flatten().shape[0] - x.flatten().shape[0])
             f.write(f"{pre_accuracy},{pre_score},{accuracy},{score},{plain_length},{adv_length}\n")
             f.flush()
+    return True
 
 
 if __name__ == "__main__":
     multiprocessing.set_start_method('spawn')
 
     with Pool(processes=num_processes) as p:
-        p.map(run_experiment, file_paths)
+
+        result = p.map(run_experiment, file_paths)
 
