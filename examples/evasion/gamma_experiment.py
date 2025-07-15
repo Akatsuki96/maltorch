7552057ffeb2e6fbc44099d99c42c08b4ae51f60
Akatsuki96 <marco.rando0396@gmail.com>
2025-07-15 16:25:51 +0200

    gamma_experiment.py: parallelized experiment execution

--- examples/evasion/gamma_experiment.py
+++ examples/evasion/gamma_experiment.py
@@ -39,7 +39,7 @@ os.makedirs(out_dir, exist_ok=True)
 
 budget = 500
 file_paths = [path for path in malware_folder.glob("*.dat")]
-
+reps_list = list(range(5))
 
 def create_model(name, threshold):
     if name == "malconv":
@@ -59,34 +59,35 @@ model = create_model(model_name, threshold)
 
 
 
-def run_experiment(file_path):
-    for r in range(reps):
-        torch.manual_seed(1123 + 143*r)
-        attack = GAMMASectionInjection(
-            query_budget=budget,
-            benignware_folder= Path(goodware_dir),#exe_folder / ".." / "benignware",
-            which_sections=[".text"],
-            random_init=True,
-            how_many_sections=num_sections
-        ) 
+def run_experiment(fpath_rep):
+    file_path, r = fpath_rep
+    # for r in range(reps):
+    torch.manual_seed(1123 + 143*r)
+    attack = GAMMASectionInjection(
+        query_budget=budget,
+        benignware_folder= Path(goodware_dir),#exe_folder / ".." / "benignware",
+        which_sections=[".text"],
+        random_init=True,
+        how_many_sections=num_sections
+    ) 
 
-        print(f"[{r}/{reps}] Attacking {file_path}...")
-        with open(f"{out_dir}/gamma_{str(file_path).split('/')[-1].split('.')[0]}_{model_name}_{num_sections}.txt", "a") as f:
-            x = load_single_exe(file_path).reshape(1, -1).long()
-            y = create_labels(x, 1, device=device)
-            dl = DataLoader(TensorDataset(x, y), batch_size=1)
-            pre_accuracy, pre_score = Accuracy()(model, dl), model(x).item()
-            print("- - - Pre-attack accuracy: ", Accuracy()(model, dl).item(), pre_score, model.threshold)
-            adv_dl = attack(model, dl)
-            accuracy = Accuracy()(model, adv_dl)
-            plain_length = x.flatten().shape[0]
-            for (x_adv, y_adv) in adv_dl:
-                score =  model(x_adv).item()
-                adv_length = x_adv.flatten().shape[0]
-                print("Score: ", score, y_adv)
-                print("ADDED BYTES", x_adv.flatten().shape[0] - x.flatten().shape[0])
-            f.write(f"{pre_accuracy},{pre_score},{accuracy},{score},{plain_length},{adv_length}\n")
-            f.flush()
+    print(f"[{r}/{reps}] Attacking {file_path}...")
+    with open(f"{out_dir}/gamma_{str(file_path).split('/')[-1].split('.')[0]}_{model_name}_{num_sections}_{r}.txt", "a") as f:
+        x = load_single_exe(file_path).reshape(1, -1).long()
+        y = create_labels(x, 1, device=device)
+        dl = DataLoader(TensorDataset(x, y), batch_size=1)
+        pre_accuracy, pre_score = Accuracy()(model, dl), model(x).item()
+        print("- - - Pre-attack accuracy: ", Accuracy()(model, dl).item(), pre_score, model.threshold)
+        adv_dl = attack(model, dl)
+        accuracy = Accuracy()(model, adv_dl)
+        plain_length = x.flatten().shape[0]
+        for (x_adv, y_adv) in adv_dl:
+            score =  model(x_adv).item()
+            adv_length = x_adv.flatten().shape[0]
+            print("Score: ", score, y_adv)
+            print("ADDED BYTES", x_adv.flatten().shape[0] - x.flatten().shape[0])
+        f.write(f"{pre_accuracy},{pre_score},{accuracy},{score},{plain_length},{adv_length}\n")
+        f.flush()
 
 
 if __name__ == "__main__":
@@ -94,5 +95,5 @@ if __name__ == "__main__":
 
     with Pool(processes=num_processes) as p:
 
-        p.map(run_experiment, file_paths)
+        p.map(run_experiment, zip(file_paths, reps_list))
 
